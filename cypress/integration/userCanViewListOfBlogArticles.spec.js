302113de02973663ad9cc5b0670f61aaa3f3103f
Becca <beccaburns89@gmail.com>
2019-11-25 08:25:35 -0500

    Creates integration spec file for user viewing a list of blogs.

---
+++ cypress/integration/userCanViewListOfBlogArticles.spec.js
@@ -0,0 +1,25 @@
+describe('User can view a list of blog articles', () => {
+
+  it('contains blog content', () => {
+    cy.get('[name="blog-1"]').within(() => {
+      cy.get('[name="blog-title"]').should('contain', 'Trip to France')
+    })
+  })
+})
+
+describe('User cannot view blogs if there are none', () => {
+
+  beforeEach(() => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/blogs',
+      response: '{"blogs":[]}'
+    })
+    cy.visit('http://localhost:3001')
+  })
+
+  it('views message for no blogs', () => {
+    cy.get("#message").should('contain', 'Oops! No blogs today.')
+  })
+})
